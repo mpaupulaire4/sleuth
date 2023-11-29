ef4f7386c5ed4a408adff6bd6c5a98075ed6cb05
Avni Prasad <avniprasad@192.168.1.25>
2023-11-29 13:44:57 +0530

    add variety to the symbols for each row

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,17 @@
   import GridCell from "./components/GridCell.svelte";
   import EditCells from "./components/EditCell.svelte";
   import ClueComp from "./components/Clue.svelte";
-  import { keys } from "./app";
+
+  export type RowDef = readonly [string[], string, string][];
+
+  export const keys: RowDef = [
+    [["1", "2", "3", "4", "5", "6"], "bg-red-300", "text-red-300"],
+    [["√", "^", "×", "÷", "+", "-"], "bg-amber-300", "text-amber-300"],
+    [["$", "€", "£", "¥", "₣", "₹"], "bg-lime-300", "text-lime-300"],
+    [["#", "%", "&", "!", "?", ";"], "bg-teal-300", "text-teal-300"],
+    [["Δ", "θ", "λ", "π", "Σ", "Ω"], "bg-blue-300", "text-blue-300"],
+    [["S", "L", "E", "U", "T", "H"], "bg-purple-300", "text-purple-300"],
+  ];
 
   const stack = new Stack();
   const can_redo = stack.can_redo;
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,10 @@
 export type RowDef = readonly [string[], string, string][];
 
 export const keys: RowDef = [
-  [["S", "L", "E", "U", "T", "H"], "bg-red-300", "text-red-300"],
-  [["S", "L", "E", "U", "T", "H"], "bg-amber-300", "text-amber-300"],
-  [["S", "L", "E", "U", "T", "H"], "bg-lime-300", "text-lime-300"],
-  [["S", "L", "E", "U", "T", "H"], "bg-teal-300", "text-teal-300"],
-  [["S", "L", "E", "U", "T", "H"], "bg-blue-300", "text-blue-300"],
+  [["1", "2", "3", "4", "5", "6"], "bg-red-300", "text-red-300"],
+  [["√", "^", "×", "÷", "+", "-"], "bg-amber-300", "text-amber-300"],
+  [["$", "€", "£", "¥", "₣", "₹"], "bg-lime-300", "text-lime-300"],
+  [["#", "%", "&", "!", "?", ";"], "bg-teal-300", "text-teal-300"],
+  [["Δ", "θ", "λ", "π", "Σ", "Ω"], "bg-blue-300", "text-blue-300"],
   [["S", "L", "E", "U", "T", "H"], "bg-purple-300", "text-purple-300"],
-  // [["S", "L", "E", "U", "T", "H"], "bg-red-300", "fill-red-300"],
-  // [["S", "L", "E", "U", "T", "H"], "bg-amber-300", "fill-amber-300"],
-  // [["S", "L", "E", "U", "T", "H"], "bg-lime-300", "fill-lime-300"],
-  // [["S", "L", "E", "U", "T", "H"], "bg-teal-300", "fill-teal-300"],
-  // [["S", "L", "E", "U", "T", "H"], "bg-blue-300", "fill-blue-300"],
-  // [["S", "L", "E", "U", "T", "H"], "bg-purple-300", "fill-purple-300"],
 ];
